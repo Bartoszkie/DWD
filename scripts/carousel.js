a2dd2c2a99d9f3ca0521753b9394af5d0503cb00
Bartoszkie <bartosz.slysz23@gmail.com>
2020-02-08 18:04:08 +0100

    added webpack for scripts, added functionall navbar

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,3 +1,28 @@
-import Glide from '@glidejs/glide'
+let opened = false;
+const mobileNavbar = document.getElementById("mobile-navbar");
 
-new Glide('.glide').mount()
+document.getElementById("hamburger").addEventListener("click", () => {
+  if (opened) {
+    opened = false;
+    mobileNavbar.style.transform = "translateY(-150%)";
+    mobileNavbar.style.opacity = "0";
+  } else {
+    opened = true;
+    mobileNavbar.style.transform = "translateY(29%)";
+    mobileNavbar.style.opacity = "1";
+  }
+});
+
+window.addEventListener("resize", () => {
+  const vw = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+  console.log(vw);
+  console.log(opened);
+  if (vw >= 900) {
+    opened = true;
+    mobileNavbar.style.transform = "translateY(29%)";
+    mobileNavbar.style.opacity = "1";
+  }
+});
