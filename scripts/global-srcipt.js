8a4c261ed7d7cfe013bd47f8840407a9bdedec69
Bartoszkie <bartosz.slysz23@gmail.com>
2020-03-18 19:12:35 +0100

    final commit with movie onclick

--- scripts/global-srcipt.js
+++ scripts/global-srcipt.js
@@ -1,6 +1,9 @@
 let opened = false;
 const mobileNavbar = document.getElementById("mobile-navbar");
 
+let videoOpen = true;
+const closeVideo = document.getElementById("video-cont");
+
 document.getElementById("hamburger").addEventListener("click", () => {
   if (opened) {
     opened = false;
@@ -25,4 +28,18 @@ window.addEventListener("resize", () => {
     mobileNavbar.style.transform = "translateY(20%)";
     mobileNavbar.style.opacity = "1";
   }
+});
+
+document.getElementById("video-close").addEventListener("click", () => {
+  if (videoOpen) {
+    videoOpen = false;
+    closeVideo.style.display = "none";
+  } else {
+    videoOpen = true;
+    closeVideo.style.display = "unset";
+  }
+});
+
+document.getElementById("header__btn").addEventListener("click", () => {
+  closeVideo.style.display = "unset";
 });
