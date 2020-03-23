5f1f2b3b5fe4c65b345a014e3cd256163499d660
Bartoszkie <bartosz.slysz23@gmail.com>
2020-03-23 20:12:46 +0100

    final commit

--- scripts/slider2.js
+++ scripts/slider2.js
@@ -1,15 +1,39 @@
-$('#lightSlider').lightSlider({
+// $('#lightSlider').lightSlider({
+//     gallery: true,
+//     item: 1,
+//     loop:true,
+//     slideMargin: 0,
+//     thumbItem: 9,
+//     pager: true,
+//     enableDrag: true,
+//     controls: true
+// });
+
+let change = false;
+
+function showGalleryDots() {
+  if ($(window).width() < 600) {
+    change = false;
+  } else {
+    change = true;
+  }
+  $("#lightSlider").lightSlider({
     gallery: true,
     item: 1,
-    loop:true,
+    loop: true,
     slideMargin: 0,
-    thumbItem: 9, 
-    pager: true
-});
+    thumbItem: 9,
+    enableDrag: true,
+    pager: true,
+    gallery: change
+  });
+}
 
-$('#goToPrevSlide').click(function(){
-    slider.goToPrevSlide(); 
+window.onload = showGalleryDots();
+
+$("#goToPrevSlide").click(function() {
+  slider.goToPrevSlide();
 });
-$('#goToNextSlide').click(function(){
-    slider.goToNextSlide(); 
+$("#goToNextSlide").click(function() {
+  slider.goToNextSlide();
 });
